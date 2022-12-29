a822b8698057f1e411ffc10639a8e6762049219a
yangzekai <1750685470@qq.com>
2022-12-29 20:13:20 +0800

    vue第二天

--- 02_vue的基础/src/main.js
+++ 02_vue的基础/src/main.js
@@ -1,4 +1,6 @@
+// 引入vue核心文件
 import { createApp } from 'vue';
+// 引入App根组件
 import App from './App.vue';
 
 /* 
@@ -10,11 +12,14 @@ import App from './App.vue';
             - 组件实例是一个Proxy对象(代理对象)
 
 */
+// 将根组件关联到应用上
 const app = createApp(App);
 
+// 将应用挂载到页面中
 const vm = app.mount("#app");
 
-// 将vm设置为全局变量
+// 我们在使用esm规范编写的变量处理闭包之中, 外部无法访问
+// 将vm设置为全局变量, 使其可以在全局被访问
 window.vm = vm;
 
 // createApp(App).mount('#app');
